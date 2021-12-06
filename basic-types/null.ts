99095ec8eac0147ed95b454df179a719ee3ef382
anuzedy <ksjqsr@gmail.com>
2021-12-06 13:33:41 +0900

    basic types

---
+++ basic-types/null.ts
@@ -0,0 +1,5 @@
+// let MyName: string = undefined;
+// let u: undefined = null;
+let v: void = undefined;
+let union: string | null = null;
+union = "Mark";
---
+++ basic-types/object.ts
@@ -0,0 +1,2 @@
+const person1 = { name: "mark", age: 39 };
+const person2 = Object.create({ name: "mark", age: 39 });
