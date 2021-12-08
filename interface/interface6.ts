315ac379f868ce8daa81ace52ad67cc11de7819a
anuzedy <ksjqsr@gmail.com>
2021-12-08 14:51:35 +0900

    interface

---
+++ interface/interface6.ts
@@ -0,0 +1,16 @@
+interface IPerson2 {
+  name: string;
+  age?: number;
+}
+
+interface IKorean extends IPerson2 {
+  city: string;
+}
+
+const k: IKorean = {
+  name: "Mark",
+  city: "Seoul",
+  age: 39,
+};
+
+HTMLDivElement;
